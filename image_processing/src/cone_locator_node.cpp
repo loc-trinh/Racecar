c35a84876a7f0f6c1cea2cfe0b5a002e37354288
Loc Trinh <loctrinh@mit.edu>
2016-02-29 15:36:19 -0500

    Added stuff

--- image_processing/src/cone_locator_node.cpp
+++ image_processing/src/cone_locator_node.cpp
@@ -57,7 +57,7 @@ ConeLocatorNode::ConeLocatorNode() : it(nh) {
 
 void ConeLocatorNode::depthCallback(const sensor_msgs::ImageConstPtr& msg){
 	try{
-		depth_image_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::CV_16UC1);
+		depth_image_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO16);
 	}
 	catch (cv_bridge::Exception& e) {
   		ROS_ERROR("cv_bridge exception: %s", e.what());
