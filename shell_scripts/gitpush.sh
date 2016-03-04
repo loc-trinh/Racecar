539080463f52646cf68409b96e597fed500d9ed1
roboteam <reobaird@mit.edu>
2016-03-03 19:30:55 -0500

    testing gpush function

--- shell_scripts/gitpush.sh
+++ shell_scripts/gitpush.sh
@@ -7,7 +7,7 @@ echo "pulling..."
 echo $(git pull)
 echo "committing..."
 echo $(git add --all)
-echo git commit -am \"$1\"
-echo $(git commit -am \"$1\")
+echo git commit -a -m "$1"
+echo $(git commit -a -m "$1")
 echo "pushing..."
 echo git push origin $branch
