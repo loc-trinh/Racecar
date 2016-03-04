33aab350bb38bea92de31a96cc57d22dd841e353
roboteam <reobaird@mit.edu>
2016-03-03 19:32:31 -0500

    testing gpush function

--- shell_scripts/gitpush.sh
+++ shell_scripts/gitpush.sh
@@ -3,10 +3,10 @@
 branch=$(git name-rev --name-only HEAD)
 direc=$(pwd)
 echo "Operating on branch $branch of git repo at: $direc"
-echo "pulling..."
+echo "--==PULLING==--"
 echo $(git pull)
-echo "committing..."
+echo "--==COMMITTING==--"
 echo $(git add --all)
 echo $(git commit -a -m "$1")
-echo "pushing..."
+echo "--==PUSHING==--"
 echo $(git push origin $branch)
