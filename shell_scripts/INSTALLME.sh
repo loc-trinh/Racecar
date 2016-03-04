aa68726990e7375a5575472f6c0a7f02ae8f71cd
roboteam <reobaird@mit.edu>
2016-03-03 22:14:46 -0500

    updating scripts

--- shell_scripts/INSTALLME.sh
+++ shell_scripts/INSTALLME.sh
@@ -1,7 +1,6 @@
-echo "Installing to ~/.bashrc" 
-echo "Please do not run more than once." 
-echo "Delete contents from end of ~/.bashrc to uninstall" 
 
+echo ""
+echo ""
 function get_bashrc_contents()
 {
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -14,7 +13,7 @@ cat << EOF
 
 export PATH=\$PATH:$ABSOLUTE_PATH
 
-alias sshcar="ssh -X racecar@192.168.100.63"
+alias sshcar="sshpass -p 'racecar@mit' ssh racecar@192.168.100.63"
 alias cdws="cd $ABSOLUTE_PATH/../"
 alias rosenv="env | grep ROS"
 alias gs="git status"
@@ -39,6 +38,28 @@ gitmakebranch() {
     git branch --set-upstream-to=origin/\$1 \$1
     cd \$CURRDIR
 }
+cm(){
+    CURRDIR="\$(pwd)"
+    cdws
+    cd ../..
+    catkin_make -j
+    cd \$CURRDIR
+}
+cmr(){
+    CURRDIR="\$(pwd)"
+    cdws
+    cd ../..
+    catkin_make -DCMAKE_BUILD_TYPE=Release
+    cd \$CURRDIR
+}
+cmc(){
+    CURRDIR="\$(pwd)"
+    cdws
+    cd ../..
+    catkin_make clean
+    catkin_make 
+    cd \$CURRDIR
+}
 alias gpush=gitpush
 alias gpull=gitpull
 alias gmb=gitmakebranch
@@ -49,7 +70,32 @@ alias rosremote=". rosenv.sh 1"
 EOF
 }
 
+check_dep() {
+	if [[ $(which $1) ]]; then
+		echo "Dependency $1 already installed." 
+	else
+		echo "Dependency $1 required. installing..." 
+		sudo apt-get install -y $1	
+	fi
+}
+
 OUT=$(get_bashrc_contents)
+
+if [ -a ~/.bashrc_bak ]; then
+	echo "Restoring original ~/.bashrc..."
+	cp ~/.bashrc_bak ~/.bashrc
+else
+	echo "Backing up ~/.bashrc -> ~/.bashrc_bak"
+	cp ~/.bashrc ~/.bashrc_bak
+fi
+
+echo "Installing to ~/.bashrc" 
+echo "Restore ~/.bashrc_bak to uninstall" 
+
 echo "$OUT" >> ~/.bashrc
 echo ""
-echo "Done installing. Changes take effect when terminal session is restarted." 
+echo "Done modifying ~/.bashrc. Changes take effect when terminal session is restarted." 
+echo ""
+echo "Now checking tools..." 
+check_dep "sshpass"
+check_dep "tmux"
--- shell_scripts/README.md
+++ shell_scripts/README.md
@@ -10,10 +10,13 @@ Installation:
 ./INSTALLME.sh
 ```
 
+Note: will install sshpass package to simplify ssh'ing into car and automatically executing commands
+
 Usage:
 ----------------
 Installation will add the following bash scripts to your environment:
 ```
+--LOCAL COMMANDS--
 cdws - cd to the racecar source folder of the catkin workspace
 sshcar - ssh to the racecar
 rosenv - print out ros environment variables
@@ -23,4 +26,9 @@ gpull - pull current branch from racecar repository root directory
 gmb $1 - make branch where branch name is first arguement. Branch is pushed to origin and upstream is set.
 roslocal - set ros environments for local/simulator usage
 rosremote - set ros environment for roscore on physical racecar
+cm - catkin_make from correct directory (accelerated)
+cmr - catkin_make from correct directory (release build)
+cmc - catkin_make from correct directory (rebuild)
+
+--REMOTE COMMANDS--
 ```
