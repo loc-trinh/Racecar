1500f32c8fb69866596d79d6419e9f0c15a0a66f
roboteam <reobaird@mit.edu>
2016-03-03 19:28:41 -0500

    test me

--- shell_scripts/gitpush.sh
+++ shell_scripts/gitpush.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-git pull
 branch=$(git name-rev --name-only HEAD)
-git add --all
-echo $(pwd)
+direc=$(pwd)
+echo "Operating on branch $branch of git repo at: $direc"
+echo "pulling..."
+echo $(git pull)
+echo "committing..."
+echo $(git add --all)
 echo git commit -am \"$1\"
+echo $(git commit -am \"$1\")
+echo "pushing..."
 echo git push origin $branch
