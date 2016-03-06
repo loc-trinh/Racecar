65e470b9a607124c65d8a495aa64e4276479e4c7
mohamoud <mohamoud@mit.edu>
2016-03-06 15:10:39 -0500

    -m

--- lab4/image_processing/scripts/scan_cone_detector.py
+++ lab4/image_processing/scripts/scan_cone_detector.py
@@ -3,22 +3,27 @@ import rospy
 import numpy as np 
 from operator import itemgetter
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Point32
+from geometry_msgs.msg import PointStamped 
+from geometry_msgs.msg import Point
 from std_msgs.msg import Float32
 from std_msgs.msg import Float64
 
 class ConeDetector:
 	def __init__(self):
-		self.cone_sub = rospy.Subscriber("cone_location", Float32, self.phi_callback)
+		#self.cone_sub = rospy.Subscriber("cone_location", Float32, self.phi_callback)
 		self.cd_sub = rospy.Subscriber("scan", LaserScan, self.laser_callback)
-		self.cd_pub = rospy.Publisher("cone_position", Point32, queue_size=4)
-		self.phi = 1000.0
+		self.cd_pub = rospy.Publisher("cone_position", PointStamped, queue_size=4)
+		self.phi = 7.0
 		self.phi_start=self.phi
 		self.phi_end = self.phi
 		self.window=3
+		self.stampedpoint=PointStamped()
+		self.counter=0
 	def phi_callback(self, msg):
 		self.phi=msg.data
 	def laser_callback(self,msg):
+		#ang="resolution:%s"%str(msg.angle_max-msg.angle_min)
+		time=rospy.Time.now()
 		if self.phi<np.pi:#check the angle
 			phi_index=int((msg.angle_max+self.phi)/(msg.angle_max-msg.angle_min)*len(msg.ranges))
 			points=msg.ranges[phi_index-self.window:phi_index+self.window]
@@ -27,7 +32,8 @@ class ConeDetector:
 			self.phi_end=self.phi+np.pi/(18+3*distance)
 			start_point=int((msg.angle_max+self.phi_start)/(msg.angle_max-msg.angle_min)*len(msg.ranges))
 			end_point=int((msg.angle_max+self.phi_end)/(msg.angle_max-msg.angle_min)*len(msg.ranges))
-			ang="resolution:%s"%str(msg.angle_max-msg.angle_min)
+			#ang="start_point, end_point:%s"%str((start_point,end_point))
+			#print ang
 			rospy.loginfo(ang)
 			points=[]
 			for i in range(start_point, end_point-5):
@@ -40,17 +46,29 @@ class ConeDetector:
 			angle=msg.angle_increment*position+msg.angle_min
 			x=dist*np.cos(angle)
 			y=dist*np.sin(angle)
-			point = Point32()
+			point = Point()
 			point.x=x
 			point.y=y
 			point.z=0.0
-			self.cd_pub.publish(point)
+			
+			self.counter+=1
+			self.stampedpoint.header.seq=self.counter
+			self.stampedpoint.header.frame_id="base_link"
+			self.stampedpoint.header.stamp=time
+			self.stampedpoint.point=point
+			self.cd_pub.publish(self.stampedpoint)
 		else:
-			point=Point32()
+			point=Point()
 			point.x=0.0
 			point.y=0.0
 			point.z=0.0
-			self.cd_pub.publish(point)
+			self.counter+=1
+			self.stampedpoint.header.seq=self.counter
+			self.stampedpoint.header.frame_id="base_link"
+			self.stampedpoint.header.stamp=time
+			self.stampedpoint.point=point
+			self.cd_pub.publish(self.stampedpoint)
+			
 if __name__=="__main__":
 	rospy.init_node("ConeDetector")
 	node=ConeDetector()
