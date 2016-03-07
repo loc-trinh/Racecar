e920fae7d5693c25ed16d9bb83e796ebccb5304b
roboteam <reobaird@mit.edu>
2016-03-07 15:54:08 -0500

    sg

--- lab4/image_processing/scripts/scan_cone_detector.py
+++ lab4/image_processing/scripts/scan_cone_detector.py
@@ -39,8 +39,8 @@ class ConeDetector:
             #distance = np.mean(points)
             #self.phi_start=self.phi-np.pi/(12+3*distance)
             #self.phi_end=self.phi+np.pi/(12+3*distance)
-            self.phi_start=self.phi-0.25
-            self.phi_end=self.phi+0.25
+            self.phi_start=self.phi-0.3
+            self.phi_end=self.phi+.05
             start_point=int((msg.angle_max+self.phi_start)/(msg.angle_max-msg.angle_min)*len(msg.ranges))
             end_point=int((msg.angle_max+self.phi_end)/(msg.angle_max-msg.angle_min)*len(msg.ranges))
             #ang="start_point, end_point:%s"%str((start_point,end_point))
