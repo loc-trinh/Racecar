b87e519f0593da3a9bb91d6516ad538cfadb1435
Loc Trinh <loctrinh@mit.edu>
2016-03-08 00:47:25 -0500

    hi

--- lab4/image_processing/scripts/cone_detector.py
+++ lab4/image_processing/scripts/cone_detector.py
@@ -35,7 +35,7 @@ class ConeDetector:
         if abs(phi) <= 1:
             scan = LaserScan()
             scan = msg
-            print msg.angle_min, msg.angle_max
+            rospy.ros_info("%d, %d" %(msg.angle_min, msg.angle_max))
             self.scan_window.publish(scan)
 
         else:
