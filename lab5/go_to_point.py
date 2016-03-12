1ebb0eb3bc7da38c79301a05ac459a3f184b5b46
Aisha Wang <aishais@mit.edu>
2016-03-11 23:54:01 -0500

    add lab 5 locally

---
+++ lab5/go_to_point.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import rospy
+import numpy as np
+import tf
+
+class GoToPointNode():
+'''
+Goes to a baselink point, regardless of original 
+
+Uses PID control
+
+Blah blah blah 
+'''
+	def __init__(self):
+		self.blah = blah
+
+
+
+
+if __name__=="__main__":
+    rospy.init_node("GoToPoint")
+    node = GoToPointNode()
+    rospy.spin()
