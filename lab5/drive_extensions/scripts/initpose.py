3c0bf3f78edfa286b3edb09372d0283ec2267548
Aisha Wang <aishais@mit.edu>
2016-03-23 14:36:00 -0400

    setting up initpose node for acml filter

--- lab5/drive_extensions/scripts/initpose.py
+++ lab5/drive_extensions/scripts/initpose.py
@@ -9,19 +9,17 @@ class TestDriveNode:
     def __init__(self):
         self.pub = '/move_base_simple/goal'
         self.drive_pub = rospy.Publisher(self.pub, PoseWithCovarianceStamped, queue_size=1)
-        self.send_goal()
+
     def send_goal(self):
         print  "sending goal"
         while not rospy.is_shutdown():
-            
             start = PoseWithCovarianceStamped()
-            goal.target_pose.pose.position.x = 0.0
-            goal.target_pose.pose.position.y = 0.0
-            goal.target_pose.pose.position.z = 0.0
-            goal.target_pose.pose.orientation.w = 0.0
-            #print goal.target_pose.pose.position.x
+            start.pose.position.x = 0.0
+            start.pose.position.y = 0.0
+            start.pose.position.z = 0.0
+            start.pose.orientation.w = 0.0
+            start.covariance = np.diag(np.diag(np.arange(36).reshape((6,6))))
             goal.target_pose.header.frame_id = 'odom'
-
             goal.target_pose.header.stamp = rospy.Time.now()
             self.drive_pub.publish(goal)
 
@@ -29,6 +27,5 @@ if __name__=="__main__":
     
     rospy.init_node('TestDriveNode')
     node=TestDriveNode()
-    
     rospy.spin()
     
