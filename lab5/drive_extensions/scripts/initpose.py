cea4f23957132d92e469b7258f539d04f4d34370
Aisha Wang <aishais@mit.edu>
2016-03-23 15:21:36 -0400

    updating initpose dummy node

--- lab5/drive_extensions/scripts/initpose.py
+++ lab5/drive_extensions/scripts/initpose.py
@@ -16,13 +16,12 @@ class TestStartNode:
             start = PoseWithCovarianceStamped()
             start.pose.pose.position = Point(0.0, 0.0, 0.0)
             start.pose.pose.orientation.w = 0.0
-            start.pose.covariance = np.diag(np.diag(np.arange(36).reshape((6,6))))
+            start.pose.covariance = [0.]*36
             start.header.frame_id = 'odom'
             start.header.stamp = rospy.Time.now()
             self.drive_pub.publish(start)
 
 if __name__=="__main__":
-    
     rospy.init_node('TestStartNode')
     node=TestStartNode()
     rospy.spin()
