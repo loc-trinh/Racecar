a07882152cbfbd0e6542eaa3cafabe52d0451ba3
Aisha Wang <aishais@mit.edu>
2016-03-23 15:14:24 -0400

    updating test node

--- lab5/drive_extensions/scripts/initpose.py
+++ lab5/drive_extensions/scripts/initpose.py
@@ -2,30 +2,28 @@
 import rospy
 import roslib
 import numpy as np
-import actionlib
-from geometry_msgs.msg import PoseStamped,PoseWithCovarianceStamped
+from geometry_msgs.msg import Point, PoseWithCovarianceStamped
 
-class TestDriveNode:
+class TestStartNode:
     def __init__(self):
-        self.pub = '/move_base_simple/goal'
+        self.pub = '/initialpose'
         self.drive_pub = rospy.Publisher(self.pub, PoseWithCovarianceStamped, queue_size=1)
+        self.send_start()
 
-    def send_goal(self):
-        print  "sending goal"
+    def send_start(self):
+        print  "sending start"
         while not rospy.is_shutdown():
             start = PoseWithCovarianceStamped()
-            start.pose.position.x = 0.0
-            start.pose.position.y = 0.0
-            start.pose.position.z = 0.0
-            start.pose.orientation.w = 0.0
-            start.covariance = np.diag(np.diag(np.arange(36).reshape((6,6))))
-            goal.target_pose.header.frame_id = 'odom'
-            goal.target_pose.header.stamp = rospy.Time.now()
-            self.drive_pub.publish(goal)
+            start.pose.pose.position = Point(0.0, 0.0, 0.0)
+            start.pose.pose.orientation.w = 0.0
+            start.pose.covariance = np.diag(np.diag(np.arange(36).reshape((6,6))))
+            start.header.frame_id = 'odom'
+            start.header.stamp = rospy.Time.now()
+            self.drive_pub.publish(start)
 
 if __name__=="__main__":
     
-    rospy.init_node('TestDriveNode')
-    node=TestDriveNode()
+    rospy.init_node('TestStartNode')
+    node=TestStartNode()
     rospy.spin()
     
