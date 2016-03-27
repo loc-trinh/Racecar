3735ebb4f6b5a68bec5bbda09b9e09a1921dcf91
Aisha Wang <aishais@mit.edu>
2016-03-27 15:32:40 -0400

    updating dummy nodes

--- 3rd_party/teb_local_planner/scripts/test_node.py
+++ 3rd_party/teb_local_planner/scripts/test_node.py
@@ -9,17 +9,21 @@ class TestDriveNode:
     def __init__(self):
         self.pub = '/move_base_simple/goal'
         self.drive_pub = rospy.Publisher(self.pub, PoseStamped, queue_size=1)
+        self.send_goal()
 
     def send_goal(self):
         print  "sending goal"
-        goal = PoseStamped()
-        goal.pose.point.position.x = 1.0
-        goal.pose.point.position.y = 1.0
-        goal.pose.point.position.z = 0.0
-        goal.pose.orientation.w = 1.0
-        goal.header.frame_id = 'odom'
-        goal.header.stamp = rospy.Time.now()
-        drive_pub.publish(goal)
+        while not rospy.is_shutdown():
+            goal = PoseStamped()
+            goal.pose.point.position.x = 1.0
+            goal.pose.point.position.y = 1.0
+            goal.pose.point.position.z = 0.0
+            goal.pose.orientation.w = 1.0
+            goal.header.frame_id = 'odom'
+            goal.header.stamp = rospy.Time.now()
+            drive_pub.publish(goal) 
 
 if __name__=="__main__":
-    send_goal()
+    rospy.init_node('TestDriveNode')
+    node=TestDriveNode()
+    rospy.spin()
