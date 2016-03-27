f05c3b18f182fa6dde499146d988c60e1a0c942d
mohamoud <mohamoud@mit.edu>
2016-03-27 15:24:50 -0400

    test

--- 3rd_party/teb_local_planner/scripts/test_node.py
+++ 3rd_party/teb_local_planner/scripts/test_node.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python
 import rospy
 import roslib
-import numpy as np
-import actionlib
+# import actionlib
 from geometry_msgs.msg import PoseStamped
-from move_base_msgs.msg import MoveBaseGoal
+# import move_base_msgs.msg
 
 class TestDriveNode:
     def __init__(self):
         self.pub = '/move_base_simple/goal'
-        self.drive_pub = rospy.Publisher(self.pub, MoveBaseGoal, queue_size=1)
-        self.send_goal()
+        self.drive_pub = rospy.Publisher(self.pub, PoseStamped, queue_size=1)
+
     def send_goal(self):
         print  "sending goal"
-        while not rospy.is_shutdown():
-            goal = MoveBaseGoal()
-            goal.target_pose.pose.position.x = 1.0
-            goal.target_pose.pose.position.y = 1.0
-            goal.target_pose.pose.position.z = 0.0
-            goal.target_pose.pose.orientation.w = np.pi/4
-            #print goal.target_pose.pose.position.x
-            goal.target_pose.header.frame_id = 'odom'
-
-            goal.target_pose.header.stamp = rospy.Time.now()
-            self.drive_pub.publish(goal)
+        goal = PoseStamped()
+        goal.pose.point.position.x = 1.0
+        goal.pose.point.position.y = 1.0
+        goal.pose.point.position.z = 0.0
+        goal.pose.orientation.w = 1.0
+        goal.header.frame_id = 'odom'
+        goal.header.stamp = rospy.Time.now()
+        drive_pub.publish(goal)
 
 if __name__=="__main__":
-    
-    rospy.init_node('TestDriveNode')
-    node=TestDriveNode()
-    
-    rospy.spin()
-    
+    try:
+        send_goal()
