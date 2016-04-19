9a72ad4633d49ea895200f67946cfbe76b5e859b
loctrinh <loctrinh@mit.edu>
2016-04-19 15:02:33 -0400

    Added log regression

--- global_planner_trajectory/test.py
+++ global_planner_trajectory/test.py
@@ -1,50 +1,31 @@
 import matplotlib.pyplot as plt
+from math import log
 
-pt1 = (0,0)
-pt3 = (5,5)
-pt2 = (.5,3)
+start = (.01, 0)
+end = (20,-20)
 
-a11 = pt1[0]**2
-a12 = pt1[0]
-a13 = 1
-a21 = pt2[0]**2
-a22 = pt2[0]
-a23 = 1
-a31 = pt3[0]**2
-a32 = pt3[0]
-a33 = 1
+sign = 1
+if end[1] > 0:
+	sign = -1
 
-det = a11*a22*a33 + a12*a23*a31 + a13*a21*a32 - a31*a22*a13 - a32*a23*a11 - a33*a21*a12
+end = (abs(end[1]),end[0])
 
-r11 = (a22*a33 - a32*a23) * 1/float(det)
-r12 = (a13*a32 - a33*a12) * 1/float(det)
-r13 = (a12*a23 - a22*a13) * 1/float(det)
-r21 = (a23*a31 - a33*a21) * 1/float(det)
-r22 = (a11*a33 - a31*a13) * 1/float(det)
-r23 = (a13*a21 - a23*a11) * 1/float(det)
-r31 = (a21*a32 - a31*a22) * 1/float(det)
-r32 = (a12*a31 - a32*a11) * 1/float(det)
-r33 = (a11*a22 - a21*a12) * 1/float(det)
 
-a = r11*pt1[1] + r12*pt2[1] + r13*pt3[1]
-b = r21*pt1[1] + r22*pt2[1] + r23*pt3[1]
-c = r31*pt1[1] + r32*pt2[1] + r33*pt3[1]
 
-start = pt1
-end = pt3
+det = log(start[0]) - log(end[0])
+a = (start[1] - end[1])/float(det)
+b = (-log(end[0])*start[1] + log(start[0])*end[1])/float(det)
+
 path = []
-
 path.append(start)
-for i in range(20):
-  dx = 1/float(20) * (end[0]-start[0])
-  path.append((dx*i + start[0], a * (dx*i+start[0])**2 + b * (dx*i+start[0]) + c))
+for i in range(1,100):
+	dx = (end[0]-start[0])/float(100)
+	x = 0+i*dx
+	y = log(x)*a + b
+	path.append((x,y))
 path.append(end)
 
-x = [i[0] for i in path]
+x = [sign * i[0] for i in path]
 y = [i[1] for i in path]
-
 plt.plot(x,y)
 plt.show()
-
-
-
