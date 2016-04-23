096042ff3db3a647f7b6d1042e1081e40c5fc326
loctrinh <loctrinh@mit.edu>
2016-04-23 15:47:06 -0400

    fixed bugs

--- gplanner/src/trajectory.cpp
+++ gplanner/src/trajectory.cpp
@@ -20,43 +20,72 @@ void GlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costm
 }
 
 bool GlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,  std::vector<geometry_msgs::PoseStamped>& plan ){
-  int sign = 1;
-  if (goal.pose.position.y > 0)
-    sign = -1;
+	double step = 20;
 
+	/* Create new begin and end node */
+	geometry_msgs::PoseStamped begin = start;
+	geometry_msgs::PoseStamped end = goal;
+	begin.pose.position.x = .01;
+	begin.pose.position.y = 0;
+	end.pose.position.x = -goal.pose.position.y;
+	end.pose.position.y = goal.pose.position.x;
 
-  /* Create new begin and end node */
-  geometry_msgs::PoseStamped begin, end;
-  begin.pose.position.x = .01;
-  begin.pose.position.y = 0;
-  end.pose.position.x = abs(goal.pose.position.y);
-  end.pose.position.y = goal.pose.position.x;
+	double deltax = abs(start.pose.position.x-end.pose.position.x);
 
-  /* Log regression */
-  double det = log(begin.pose.position.x) - log(end.pose.position.x);
-  double a = (begin.pose.position.y - end.pose.position.y) / det;
-  double b = (-log(end.pose.position.x)*begin.pose.position.y + log(begin.pose.position.x)*end.pose.position.y) / det;
+	if (deltax < 1){
+		plan.push_back(start);
+		for(int i = 0; i < step; i++){
+			double dx = (end.pose.position.x-start.pose.position.x)/float(step);
+			double m = float(end.pose.position.y-start.pose.position.y)/(end.pose.position.x-start.pose.position.x);
+			geometry_msgs::PoseStamped point = goal;
+		    tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
+		    point.pose.orientation.x = goal_quat.x();
+		    point.pose.orientation.y = goal_quat.y();
+		    point.pose.orientation.z = goal_quat.z();
+		    point.pose.orientation.w = goal_quat.w();
 
+		    point.pose.position.x = i * dx;
+		    point.pose.position.y = m * point.pose.position.x;
+			plan.push_back(point);
+		}
+		plan.push_back(end);
+	}
+	else{
+		int sign = 1;
+		if (end.pose.position.x < 0){
+			end.pose.position.x = -end.pose.position.x;
+			sign = -1;
+		}
 
-  /* Generating path */
-  plan.push_back(start);
-  for (int i=1; i<5; i++){
-    geometry_msgs::PoseStamped point = goal;
-    tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
-    point.pose.orientation.x = goal_quat.x();
-    point.pose.orientation.y = goal_quat.y();
-    point.pose.orientation.z = goal_quat.z();
-    point.pose.orientation.w = goal_quat.w();
+		/* Log regression */
+		double det = log(begin.pose.position.x) - log(end.pose.position.x);
+		double a = (begin.pose.position.y - end.pose.position.y) / det;
+		double b = (-log(end.pose.position.x)*begin.pose.position.y + log(begin.pose.position.x)*end.pose.position.y) / det;
 
-    double dx = (end.pose.position.x-begin.pose.position.x) * .2;
-    double x = i * dx;
-    point.pose.position.y = -x * sign;
-    point.pose.position.x = log(x) * a + b;
-    //point.pose.position.x = min(point.pose.position.x,0.0);
-    plan.push_back(point);
-  }
-  plan.push_back(goal);
-  base_local_planner::publishPlan(plan, global_plan_pub_); 
-  return true;
+		/* Generating path */
+		for (int i=1; i < step; i++){
+			geometry_msgs::PoseStamped point = goal;
+			tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
+			point.pose.orientation.x = goal_quat.x();
+			point.pose.orientation.y = goal_quat.y();
+			point.pose.orientation.z = goal_quat.z();
+			point.pose.orientation.w = goal_quat.w();
+
+			double dx = (end.pose.position.x-begin.pose.position.x) * 1 / step;
+			double x = i*dx;
+			point.pose.position.y = x * sign;
+			point.pose.position.x = log(x) * a + b;
+			plan.push_back(point);
+		}
+		end.pose.position.x = -end.pose.position.x;
+		plan.push_back(end);
+	}
+	for(int i = 0; i < plan.size(); i++){
+		double temp = plan[i].pose.position.x;
+		plan[i].pose.position.x = -plan[i].pose.position.y;
+		plan[i].pose.position.y = temp;
+	}
+	base_local_planner::publishPlan(plan, global_plan_pub_); 
+	return true;
   }
 };
