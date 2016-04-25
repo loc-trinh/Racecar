ceca3c8fa399b5e2ebcb03c05001f79b865de672
loctrinh <loctrinh@mit.edu>
2016-04-25 17:47:05 -0400

    fix

--- gplanner/src/trajectory.cpp
+++ gplanner/src/trajectory.cpp
@@ -19,9 +19,6 @@ GlobalPlanner::GlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap
 void GlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
   ros::NodeHandle nh("~/" + name);
   global_plan_pub_ = nh.advertise<nav_msgs::Path>("global_plan", 1);
-  //tfBuffer = new tf2_ros::Buffer();
-  //listener = new tf2_ros::TransformListener(tfBuffer);
-  
 }
 
 bool GlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,  std::vector<geometry_msgs::PoseStamped>& plan ){
@@ -53,85 +50,72 @@ bool GlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geom
 	listener.waitForTransform("base_link", goal.header.frame_id, ros::Time(0), ros::Duration(10.0) );
 	listener.transformPose("base_link", goal, end);
 	*/
-	
-	
-	begin.pose.position.x = .01;
-	begin.pose.position.y = 0;
 
-	double temp = end.pose.position.x;
-	end.pose.position.x = -end.pose.position.y;
-	end.pose.position.y = temp;
-
-	double deltax = abs(start.pose.position.x-end.pose.position.x);
-
+	double deltax = abs(begin.pose.position.x-end.pose.position.x);
+	tf2::doTransform(begin, begin, transform);
+	plan.push_back(begin);
 
  	//Straight Line
 	if (deltax < 1){
-		tf2::doTransform(begin, begin, transform);
-		plan.push_back(begin);
 		for(int i = 0; i < step; i++){
-			double dx = (end.pose.position.x-start.pose.position.x)/float(step);
-			double m = float(end.pose.position.y-start.pose.position.y)/(end.pose.position.x-start.pose.position.x);
-			geometry_msgs::PoseStamped point = goal;
+			double dy = (end.pose.position.y-start.pose.position.y)/float(step);
+			double m = float(end.pose.position.x-start.pose.position.x)/(end.pose.position.y-start.pose.position.y);
+			
+			geometry_msgs::PoseStamped point = end;
 		    tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
 		    point.pose.orientation.x = goal_quat.x();
 		    point.pose.orientation.y = goal_quat.y();
 		    point.pose.orientation.z = goal_quat.z();
 		    point.pose.orientation.w = goal_quat.w();
 
-		    point.pose.position.x = i * dx;
+		    point.pose.position.x = begin.pose.position.y i * dy;
 		    point.pose.position.y = m * point.pose.position.x;
 
 		    //listener.transformPose("odom", point, point);
 		    tf2::doTransform(point, point, transform);
 			plan.push_back(point);
 		}
-	    for(int i = 0; i < plan.size(); i++){
-	      double temp = plan[i].pose.position.x;
-	      plan[i].pose.position.x = plan[i].pose.position.y;
-	      plan[i].pose.position.y = -temp;
-	    }
-	    tf2::doTransform(end, end, transform);
-		plan.push_back(end);
 	}
-  // Curved Line
-	else{
-		int sign = 1;
-		if (end.pose.position.x < 0){
-			end.pose.position.x = -end.pose.position.x;
-			sign = -1;
-		}
+	tf2::doTransform(end, end, transform);
+	plan.push_back(end);
+ //  // Curved Line
+	// else{
+	// 	int sign = 1;
+	// 	if (end.pose.position.x < 0){
+	// 		end.pose.position.x = -end.pose.position.x;
+	// 		sign = -1;
+	// 	}
 
-		/* Log regression */
-		double det = log(begin.pose.position.x) - log(end.pose.position.x);
-		double a = (begin.pose.position.y - end.pose.position.y) / det;
-		double b = (-log(end.pose.position.x)*begin.pose.position.y + log(begin.pose.position.x)*end.pose.position.y) / det;
+	// 	/* Log regression */
+	// 	double det = log(begin.pose.position.x) - log(end.pose.position.x);
+	// 	double a = (begin.pose.position.y - end.pose.position.y) / det;
+	// 	double b = (-log(end.pose.position.x)*begin.pose.position.y + log(begin.pose.position.x)*end.pose.position.y) / det;
 
-		/* Generating path */
-		tf2::doTransform(begin, begin, transform);
-		plan.push_back(begin);
-		for (int i=1; i < step; i++){
-			geometry_msgs::PoseStamped point = goal;
-			tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
-			point.pose.orientation.x = goal_quat.x();
-			point.pose.orientation.y = goal_quat.y();
-			point.pose.orientation.z = goal_quat.z();
-			point.pose.orientation.w = goal_quat.w();
+	// 	/* Generating path */
+	// 	tf2::doTransform(begin, begin, transform);
+	// 	plan.push_back(begin);
+	// 	for (int i=1; i < step; i++){
+	// 		geometry_msgs::PoseStamped point = goal;
+	// 		tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
+	// 		point.pose.orientation.x = goal_quat.x();
+	// 		point.pose.orientation.y = goal_quat.y();
+	// 		point.pose.orientation.z = goal_quat.z();
+	// 		point.pose.orientation.w = goal_quat.w();
 
-			double dx = (end.pose.position.x-begin.pose.position.x) * 1 / step;
-			double x = i*dx;
-			point.pose.position.y = -x * sign;
-			point.pose.position.x = log(x) * a + b;
+	// 		double dx = (end.pose.position.x-begin.pose.position.x) * 1 / step;
+	// 		double x = i*dx;
+	// 		point.pose.position.y = -x * sign;
+	// 		point.pose.position.x = log(x) * a + b;
 
-		    tf2::doTransform(point, point, transform);
-			plan.push_back(point);
-		}
-		tf2::doTransform(end, end, transform);
-		double temp = end.pose.position.x;
-		end.pose.position.x = end.pose.position.y;
-		end.pose.position.y = -temp;
-		plan.push_back(end);
-	}
+	// 	    tf2::doTransform(point, point, transform);
+	// 		plan.push_back(point);
+	// 	}
+	// 	tf2::doTransform(end, end, transform);
+	// 	double temp = end.pose.position.x;
+	// 	end.pose.position.x = end.pose.position.y;
+	// 	end.pose.position.y = -temp;
+	// 	plan.push_back(end);
+	// }
 	
 	base_local_planner::publishPlan(plan, global_plan_pub_); 
 	return true;
