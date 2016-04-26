6211655e8e55e8260f7f873d675ffc6c35482bd0
Roboteam <jamam@mit.edu>
2016-04-25 20:28:17 -0400

    optimizing curve planner

--- _base/racecar/scripts/scan_processor2.py
+++ _base/racecar/scripts/scan_processor2.py
@@ -9,8 +9,8 @@ class HokuyoScanProcessor:
 
 	def callback(self, msg):
 		data = [0.0] * len(msg.intensities)
-		for i in range(0,len(msg.intensities)):
-			if msg.intensities[i] < 0.1:
+		for i in range(10,len(msg.intensities) - 10):
+			if msg.intensities[i] < 0.1 and msg.intensities[i-2] < 0.1 and msg.intensities[i-1] < 0.1 and msg.intensities[i+1] < 0.1 and msg.intensities[i+2] < 0.1:
 				data[i] = 100;
 			else:
 				data[i] = msg.intensities[i];
@@ -18,8 +18,8 @@ class HokuyoScanProcessor:
 		msg.intensities = data;
 
 		data = [0.0] * len(msg.ranges)
-		for i in range(0,len(msg.ranges)):
-			if msg.ranges[i] > 30:
+		for i in range(10,len(msg.ranges) - 10):
+			if msg.ranges[i] > 30 and msg.ranges[i-2] > 30 and msg.ranges[i-1] > 30 and msg.ranges[i+1] > 30 and msg.ranges[i+2] > 30:
 				data[i] = 29.0;
 			else:
 				data[i] = msg.ranges[i];
