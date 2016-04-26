4569ff427481e56556df5f9fda7ed2297e6073ae
Roboteam <jamam@mit.edu>
2016-04-25 21:06:42 -0400

    optimizing curve planner

--- freespace_planner/src/freespace_planner.py
+++ freespace_planner/src/freespace_planner.py
@@ -197,7 +197,7 @@ if __name__ == "__main__":
     
     # enter the ROS main loop
     
-    rate = rospy.Rate(0.333)
+    rate = rospy.Rate(2)
     while not rospy.is_shutdown():
         Planner.perform_update();
         rate.sleep()
