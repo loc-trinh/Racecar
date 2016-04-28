d47604f421ec30d74b9e51c678ddb8fdb57de941
Aisha Wang <aishais@mit.edu>
2016-04-27 21:27:42 -0400

    framework and type stuffs and pseudocode for the baby rrt

--- alt_local_planner.cpp
+++ alt_local_planner.cpp
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <ros/ros.h>
 
+// includes for ROS message types
+#include "nav_msgs/OccupancyGrid.h"
+#include "std_msgs/Header.h"
+#include "nav_msgs/MapMetaData.h"
+
+#include <vector>
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs_vector.h"
+
 using namespace std;
 
-// Alternative local planner
-// Input: 
-//		trajectory (vector of points) from global planner 
-//		local cost map (lidar data)
-// Output:
-// 		trajectory to goal points, modified for obstacles
+// Alternative local planner (Baby RRT)
+// Subscribes: 
+//		trajectory (vector of 10 PoseStamped) from Loc
+//		local cost map (lidar data) from costmap node (Loc)
+// Publishes:
+// 		trajectory vector of 10 PostStamped msgs
+// Algorithm:
 //		(stretch) add in cost computation for colored circle detection
 
-// Suggested logic - detect obstacle and then plot a course that just goes right next to it
-// Or a discrete version of TEB? aka draw a line, find obstacles, and keep moving around, then sample it back?
-// Or could create two nodes that subscribe and funnel into a final node that passes everything into my node lawl
+// Need to make sure node spins at certain frequency, instead of working off of callbacks
+
+// void ConeLocatorNode::locationCallback(const sensor_msgs::ImageConstPtr& msg){
 
 class AltLocalPlannerNode { 
 public:
@@ -23,35 +33,51 @@ private:
 	ros::NodeHandle nh;
 
 	// pubs-subs
-
-	// Update this to receive a trajectory (vector of points) from Loc
 	ros::Subscriber globalPathSub;
-
-	// Update this to receive a cost map from global planner
 	ros::Subscriber localCostMapSub;
-
-	// Update this to link into the direct ackermann commands
-	ros::Publisher localDriveComdsPub;
-
-	// Double check how to chain together multiple subscriptions, store cost map as local var?
-	// or lock it up?
+	ros::Publisher localPathPub;
 
 	// callbacks
-	void imageCallback(const sensor_msgs::ImageConstPtr& msg);
-	void someOtherCallback(const );
-};
+	// update when figure out what type the trajectory is
+	void trajectoryCallback(const sensor_msgs::ImageConstPtr& msg);
+	void costMapCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg);
+
+	// locals
+	nav_msgs::OccupancyGrid::ConstPtr localCostMapPtr;
+	vector<geometry_msgs::PoseStamped::ConstPtr> trajPtr;
 
 AltLocalPlannerNode::AltLocalPlannerNode() : it(nh) {
-	globalPathSub = it.subscribe("/??", 1, &AltLocalPlannerNode::someCallback, this);
-	localCostMapSub = it.subscribe("??", 1, &AltLocalPlannerNode::someOtherCallback, this);
+	globalPathSub = it.subscribe("/somewhere", 1, &AltLocalPlannerNode::trajectoryCallback, this);
+	localCostMapSub = it.subscribe("overtherainbow/grid", 1, &AltLocalPlannerNode::costMapCallback, this);
 	localDriveComdsPub = it.advertise("localDriveCommands", 1);
 }
 
+void AltLocalPlannerNode::trajectorycallback(const package_name::geometry_msgs_vector::ConstPtr &msg) {
+}
 
+void AltLocalPlannerNode::costMapCallback(const sensor_msgs::ImageConstPtr& msg){
+
+}
+
+/* Params:
+	stepSize: defaults to Euclidean distance between first two pts in trajectory
+	branchFactor: defaults to 5 new waypoints to consider
+
+	Algorithm:
+	Similar to RRT, takes in trajectory and checks if each point is valid and the
+	straight line path there is valid. If not, build a tree that allows the car to go
+	forward/around obstacle, then take each of these new trajectories, and try
+	the next point in the trajectory, iterate until find valid path to goal point.
+*/
+void AltLocalPlannerNode::pathPlanning("blah"){
+
+}
 
 int main(int argc, char *argv[]) {
 	ros::init(argc, argv, "AltLocalPlannerNode");
 	AltLocalPlannerNode node;
+	ros::Rate r(60); // 60 hz
+	// call the planning func here?
 	ros::spin();
 	return 0; 
 }
