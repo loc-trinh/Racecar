e78c53797c09dbf7825198f88f903c1dd79dd738
Aisha Wang <aishais@mit.edu>
2016-04-29 01:46:23 -0400

    committing before shutdown, working through pseudocode/unknown methodology

--- alt_local_planner.cpp
+++ alt_local_planner.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <ros/ros.h>
+#include <iostream>
+#include <vector>
+#include <tuple>
 
 // includes for ROS message types
 #include "nav_msgs/OccupancyGrid.h"
@@ -7,14 +10,15 @@
 #include "nav_msgs/MapMetaData.h"
 
 #include <vector>
+// #include "geometry_msgs/PoseArray.h"
+#include "nav_msgs/Path.h"
 #include "geometry_msgs/PoseStamped.h"
-#include "geometry_msgs_vector.h"
 
 using namespace std;
 
 // Alternative local planner (Baby RRT)
 // Subscribes: 
-//		trajectory (vector of 10 PoseStamped) from Loc
+//		trajectory (array of 10 PoseStamped) from Loc
 //		local cost map (lidar data) from costmap node (Loc)
 // Publishes:
 // 		trajectory vector of 10 PostStamped msgs
@@ -22,8 +26,6 @@ using namespace std;
 //		(stretch) add in cost computation for colored circle detection
 
 // Need to make sure node spins at certain frequency, instead of working off of callbacks
-
-// void ConeLocatorNode::locationCallback(const sensor_msgs::ImageConstPtr& msg){
 
 class AltLocalPlannerNode { 
 public:
@@ -39,12 +41,12 @@ private:
 
 	// callbacks
 	// update when figure out what type the trajectory is
-	void trajectoryCallback(const sensor_msgs::ImageConstPtr& msg);
+	void trajectoryCallback(const nav_msgs::Path& msg);
 	void costMapCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg);
 
 	// locals
 	nav_msgs::OccupancyGrid::ConstPtr localCostMapPtr;
-	vector<geometry_msgs::PoseStamped::ConstPtr> trajPtr;
+	nav_msgs::Path trajPtr;
 
 AltLocalPlannerNode::AltLocalPlannerNode() : it(nh) {
 	globalPathSub = it.subscribe("/somewhere", 1, &AltLocalPlannerNode::trajectoryCallback, this);
@@ -52,14 +54,22 @@ AltLocalPlannerNode::AltLocalPlannerNode() : it(nh) {
 	localDriveComdsPub = it.advertise("localDriveCommands", 1);
 }
 
-void AltLocalPlannerNode::trajectorycallback(const package_name::geometry_msgs_vector::ConstPtr &msg) {
+void AltLocalPlannerNode::trajectorycallback(const geometry_msgs_vector::ConstPtr &msg) {
+	// not sure how to store the info from a pointer so that it can be used by the class?
+	trajPtr = msg.data;
+
+	// change possible array type to vector?
+	// tf transform the posestamped into base_link?
 }
 
-void AltLocalPlannerNode::costMapCallback(const sensor_msgs::ImageConstPtr& msg){
+void AltLocalPlannerNode::costMapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg){
+	// again, not sure how to store things in C++?
+	localCostMapPtr = msg.data;
 
+	// do the tf transforms here on the occupancygrid?
 }
 
-/* Params:
+/* Params: not sure how to do params in C++? http://wiki.ros.org/roscpp/Overview/Parameter%20Server
 	stepSize: defaults to Euclidean distance between first two pts in trajectory
 	branchFactor: defaults to 5 new waypoints to consider
 
@@ -68,8 +78,43 @@ void AltLocalPlannerNode::costMapCallback(const sensor_msgs::ImageConstPtr& msg)
 	straight line path there is valid. If not, build a tree that allows the car to go
 	forward/around obstacle, then take each of these new trajectories, and try
 	the next point in the trajectory, iterate until find valid path to goal point.
+
+	how to pass in cost map/locations?
 */
-void AltLocalPlannerNode::pathPlanning("blah"){
+void AltLocalPlannerNode::pathPlanning(OccupancyGrid map, Path traj){
+	// v is a vector of PostStamped
+	int delta = v.at(1).data.pose.point.x - v.at(0).data.pose.point.x
+	tuple<int,int,int> point (10,10,10);
+
+	// check on this param
+	int branchFactor = 5;
+	std::vector<tuple<int,int,int>> myvector (branchFactor);
+
+	// threshold should be a param? currently set in config file?
+	int threshold = 0.35;
+
+	for(std::vector<T>::iterator it = v.begin(); it != v.end(); ++it) {
+    	// std::cout << *it;
+
+    	// check current point's value in occupancy grid
+    	// if invalid, branch, else, move to next point
+    	grid_x = (unsigned int)((*it.data.pose.point.x - map.info.origin.position.x) / map.info.resolution);
+    	grid_y = (unsigned int)((*it.data.pose.point.y - map.info.origin.position.y) / map.info.resolution);  
+		int occ_val = map[grid_x * width + grid_y];
+
+		if (occ_val > threshold):
+
+     // how to access costmap data?
+     // if point in invalid part of cost map,
+     // sample points in range, delta ahead of car
+     // prune these points,
+     // append to an array
+     // 
+
+
+
+
+	}
 
 }
 
