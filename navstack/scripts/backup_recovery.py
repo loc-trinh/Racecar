4d85e35a51393a11966cc8725e24ae0ad2903d07
Roboteam <jamam@mit.edu>
2016-04-30 22:49:27 -0400

    add plan_Executor

--- navstack/scripts/backup_recovery.py
+++ navstack/scripts/backup_recovery.py
@@ -140,7 +140,7 @@ class BackupRecovery:
 
     def getIndex(self,x,y):
         sx = self.grid.info.width;
-        return int(y * sx + x);
+        return int(round(y * sx + x));
 
     def costmap_update_callback(self,data):
         if self.grid == None:
