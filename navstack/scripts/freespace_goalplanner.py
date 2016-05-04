f5685549f7332e36bcd1170238f9f9f3253e3051
loctrinh <loctrinh@mit.edu>
2016-05-04 16:27:03 -0400

    tuning

--- navstack/scripts/freespace_goalplanner.py
+++ navstack/scripts/freespace_goalplanner.py
@@ -199,7 +199,7 @@ if __name__ == "__main__":
     
     # enter the ROS main loop
     
-    rate = rospy.Rate(20)
+    rate = rospy.Rate(50)
     while not rospy.is_shutdown():
         Planner.perform_update();
         rate.sleep()
