8fe37fae0d4f6c21b4d231ba59ed50dc98477694
Roboteam <jamam@mit.edu>
2016-05-07 15:56:55 -0400

    tuning

--- navstack/scripts/backup_recovery.py
+++ navstack/scripts/backup_recovery.py
@@ -119,7 +119,7 @@ class BackupRecovery:
         if stop:
             self.collide = min(self.collide+1, 60);
         else:
-            self.collide = min(self.collide-5, 0);
+            self.collide = max(self.collide-5, 0);
         print self.collide;
         if self.collide > 30:
             self.perform_backup_move();
